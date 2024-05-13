939ed2a6e61c9914d374a7dddda6fb2dd09c7ab0
AlinaLokkinen <bhd524@myy.haaga-helia.fi>
2024-05-13 09:52:02 +0300

    lisää hiomista kun gh pages ei toimi

--- AAVA-projekti/vite.config.js
+++ AAVA-projekti/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/aava-projekti',
+  base: '/aava_frontend/',
   plugins: [react()],
 })
